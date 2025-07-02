6f9c2b36ae66bb10efa68cb3fa2b5a1b7b35bc18
NomadNiko <nomad.niko.inc@gmail.com>
2025-07-02 07:37:47 -1000

    fix: fixing

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -77,7 +77,8 @@ export class UsersController {
   })
   @Get('search')
   @HttpCode(HttpStatus.OK)
-  @UseGuards(AuthGuard('jwt')) // Override class-level admin restriction - allow all authenticated users
+  @Roles() // Clear the admin role requirement
+  @UseGuards(AuthGuard('jwt')) // Override class-level guards - allow all authenticated users
   async searchUsers(@Query('q') searchTerm: string): Promise<UserSchemaClass[]> {
     return this.usersService.searchUsers(searchTerm || '');
   }
